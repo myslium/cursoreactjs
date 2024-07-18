61aeca23bb783c9f30404fef4546b0d3c167aa6c
Myslium <mazzinoob@gmail.com>
2024-07-18 15:37:18 -0300

    Estilizando o app e contato

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -5,13 +5,34 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="pagina-app">
-     <h1>Estudos de ReactJs
-     <i className='fa fa-heart'></i>
-     </h1>
 
-     <img className='foto'  src="/assets/images/ft.react.png" />
+      <header className='cabecalho'> 
+         <h1>Estudos de ReactJs
+         <i className='fa fa-heart'></i>
+         </h1>
+      </header>
+
+      <section className='secao'>
+        <h1>Estudando ReactJS</h1>
+
+        <input type = 'text' placeholder='Digite aqui'/>
+        <br /><br />
+        <select>
+          <option>Item 1</option>
+          <option>Item 2</option>
+
+        </select>
+        <br /><br />
+
+        <button> Clique aqui </button>
+
+        <ul>
+          <li>
+          <Link to = '/contato'> Ir para Contato</Link>
+          </li>
+        </ul>
+      </section>
     
-    <h2 className='linkando'> <Link to = '/contato'> Ir para Contato</Link></h2>
     </div>
   );
 }
--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -8,10 +8,14 @@ export default function Contato() {
     return (
 
         <div className='pagina-contato'>
+
+            <header className='cabecalho'>
                 <Link to = '/'>
             <i className='fa fa-arrow-left'></i>
             </Link> 
             <h1>Contato</h1>
+            </header>
+            <img src = '/assets/images/ftcontato.png'/>
         </div>
     )
 }
